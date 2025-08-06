d450eff77959a51376fdebfc901d3c250536351e
CamiloHernandez2005 <ahernandez@emida.com>
2025-08-06 11:35:55 -0500

    first commit

---
+++ QATest_Back/src/main/java/com/example/AutomatizationQA/AutomatizationQaApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutomatizationQA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizationQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatizationQaApplication.class, args);
+	}
+
+}
---
+++ QATest_Back/src/test/java/com/example/AutomatizationQA/AutomatizationQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutomatizationQA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatizationQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
